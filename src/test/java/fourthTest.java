526693cf8ed7082317f107078bb24ce3b17f8dac
Hakan Gok <71521573+gokhakan@users.noreply.github.com>
2021-12-30 10:59:12 +0000

    Update fourthTest.java
    new print line added

--- src/test/java/fourthTest.java
+++ src/test/java/fourthTest.java
@@ -1,5 +1,6 @@
 public class fourthTest {
     public static void main(String[] args) {
         System.out.println("this is the fourth test updated");
+        System.out.println("this is a new line");
     }
 }
