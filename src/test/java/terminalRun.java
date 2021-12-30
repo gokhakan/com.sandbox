e17484ed03d8bb111507f5cf10381dcb386f000d
Hakan Gok <71521573+gokhakan@users.noreply.github.com>
2021-12-30 12:38:00 +0000

    Update terminalRun.java
    something added

--- src/test/java/terminalRun.java
+++ src/test/java/terminalRun.java
@@ -1,5 +1,5 @@
 public class terminalRun {
     public static void main(String[] args) {
-        System.out.println("Terminal run is successful!");
+        System.out.println("Terminal run is successful! again");
     }
 }
