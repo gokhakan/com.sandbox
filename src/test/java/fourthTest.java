1461f51598ec666e1ee97379e626b9145e1972d1
gokhakan <hakangok@icloud.com>
2021-12-30 10:55:12 +0000

    ready for actions

--- src/test/java/fourthTest.java
+++ src/test/java/fourthTest.java
@@ -1,5 +1,5 @@
 public class fourthTest {
     public static void main(String[] args) {
-        System.out.println("this is the fourth test");
+        System.out.println("this is the fourth test updated");
     }
 }
